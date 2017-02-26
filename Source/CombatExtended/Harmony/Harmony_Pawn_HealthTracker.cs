7b745cbfb666ece505fb9f3966d210486f38265e
ProfoundDarkness <prodark77@gmail.com>
2017-02-26 07:56:22 -0700

    Attempted to disable the random kill of pawns in CheckForStateChange via harmony.
    Looking at the logic I noticed the whole condition is based on if a random value is < num.  So I changed num to 0.
    
    Was the easiest way for me to attack the logic via IL but now that I comprehend a few more things I think I might be able to remove the whole conditional check (faster code execution).

---
+++ Source/CombatExtended/Harmony/Harmony_Pawn_HealthTracker.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using Harmony;
+using Verse;
+
+namespace CombatExtended.Harmony
+{
+	[StaticConstructorOnStartup]
+	static class Main
+	{
+		static Main()
+		{
+			var harmony = HarmonyInstance.Create("CombatExtended.Harmony.Pawn_HealthTracker");
+			harmony.PatchAll(Assembly.GetExecutingAssembly());
+		}
+		
+		[HarmonyPatch(typeof(Pawn_HealthTracker))]
+		[HarmonyPatch("CheckForStateChange")]
+		static class Disable_RNG_Death
+		{
+			public class RNGReplacer : CodeProcessor
+			{
+				int countReplace = 0;
+				
+				public override List<CodeInstruction> Process(CodeInstruction instruction)
+				{
+					if (instruction == null) return null;
+					//Log.Message(string.Concat("Instruction: ", instruction.opcode.ToString(), " :: ", instruction.operand == null ? "null" : instruction.operand.ToString()));
+					if (countReplace < 2 && instruction.opcode == OpCodes.Ldc_R4)
+					{
+						instruction.operand = 0.0f;
+						countReplace++;
+						//Log.Message(string.Concat("Changed to: ", instruction.opcode.ToString(), " :: ", instruction.operand == null ? "null" : instruction.operand.ToString()));
+					}
+					return new List<CodeInstruction> { instruction };
+				}
+			}
+			
+			[HarmonyProcessorFactory]
+			static HarmonyProcessor Disable_RNG_Death_Patch(MethodBase original)
+			{
+				var processor = new HarmonyProcessor();
+				processor.Add(new RNGReplacer());
+				return processor;
+			}
+		}
+	}
+}
